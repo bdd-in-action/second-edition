8e3b7199f61da9634282c98fcea3ed88b35cacb8
John Ferguson Smart <john.smart@wakaleo.com>
2020-08-30 19:47:57 +0100

    Renamed folders

--- chapter-1/java/online-banking/src/test/resources/features/transfers/readme.md
+++
@@ -1,3 +0,0 @@
-Transfers
-
-Give online clients the ability to transfer funds between their own accounts and to other accounts both within the bank and between banks.
--- chapter-2/java/train-timetables/src/test/java/manning/bddinaction/acceptancetests/AcceptanceTestSuite.java
+++
@@ -1,9 +0,0 @@
-package manning.bddinaction.acceptancetests;
-
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/", glue="manning.bddinaction")
-public class AcceptanceTestSuite {}
